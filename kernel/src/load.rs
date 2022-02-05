83019fa0ece77e98fc324edd2c247cecc84d79a2
mrzleo <mr.zleo10@gmail.com>
2022-02-05 11:50:35 +0800

    support loading multiple apps in memory at the same time

--- kernel/src/load.rs
+++ kernel/src/load.rs
@@ -2,6 +2,34 @@ use crate::config::*;
 use crate::trap::TrapContext;
 use core::arch::asm;
 
+#[derive(Clone, Copy)]
+#[repr(align(4096))]
+struct Stack {
+    data: [u8; KERNEL_STACK_SIZE],
+}
+
+static KERNEL_STACK: [Stack; MAX_APP_NUM] = [Stack {
+    data: [0; KERNEL_STACK_SIZE],
+}; MAX_APP_NUM];
+
+static USER_STACK: [Stack; MAX_APP_NUM] = [Stack {
+    data: [0; USER_STACK_SIZE],
+}; MAX_APP_NUM];
+
+impl Stack {
+    pub fn sp(&self) -> usize {
+        self.data.as_ptr() as usize + self.data.len()
+    }
+
+    pub fn push_ctx(&self, cxt: TrapContext) -> usize {
+        let cxt_ptr = (self.sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
+        unsafe {
+            *cxt_ptr = cxt;
+        }
+        cxt_ptr as usize
+    }
+}
+
 extern "C" {
     fn _num_app();
 }
@@ -17,11 +45,11 @@ fn num_app() -> usize {
 }
 
 #[inline]
-fn base_i(app_id: usize) -> usize {
+fn base(app_id: usize) -> usize {
     APP_BASE_ADDR + app_id * APP_SIZE_LIMIT
 }
 
-fn load_app() {
+pub fn load_app() {
     let apps_ptr = apps_ptr();
     let num_app = num_app();
     let app_start = unsafe { core::slice::from_raw_parts(apps_ptr.add(1), num_app + 1) };
@@ -31,7 +59,7 @@ fn load_app() {
     }
 
     for i in 0..num_app {
-        let base_i = base_i(i);
+        let base_i = base(i);
         (base_i..base_i + APP_SIZE_LIMIT)
             .for_each(|addr| unsafe { (addr as *mut u8).write_volatile(0) });
 
@@ -45,29 +73,9 @@ fn load_app() {
     }
 }
 
-#[repr(align(4096))]
-struct Stack {
-    data: [u8; KERNEL_STACK_SIZE],
-}
-
-static KERNEL_STACK: Stack = Stack {
-    data: [0; KERNEL_STACK_SIZE],
-};
-
-static USER_STACK: Stack = Stack {
-    data: [0; USER_STACK_SIZE],
-};
-
-impl Stack {
-    pub fn sp(&self) -> usize {
-        self.data.as_ptr() as usize + self.data.len()
-    }
-
-    pub fn push_ctx(&self, ctx: TrapContext) -> &'static mut TrapContext {
-        let ctx_ptr = (self.sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
-        unsafe {
-            *ctx_ptr = ctx;
-            ctx_ptr.as_mut().unwrap()
-        }
-    }
+pub fn init_app_cxt(app_id: usize) -> usize {
+    KERNEL_STACK[app_id].push_ctx(TrapContext::app_init_cxt(
+        base(app_id),
+        USER_STACK[app_id].sp(),
+    ))
 }
