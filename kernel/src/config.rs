033cc40d3537f1de70a00fe441185976cd3e339f
mrzleo <mr.zleo10@gmail.com>
2022-03-08 15:32:14 +0800

    modify memory configuration

--- kernel/src/config.rs
+++ kernel/src/config.rs
@@ -13,7 +13,7 @@ pub const PAGE_SIZE: usize = 4096;
 pub const PAGE_OFFSET: usize = 12;
 
 // memory
-pub const MEMORY_END: usize = 0x80f0_0000;
+pub const MEMORY_END: usize = 0x8800_0000; // 128M Memory
 pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
 
